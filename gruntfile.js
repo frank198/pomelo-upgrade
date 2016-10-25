319d66501c9abdd9e6ca0171742019e6ad8bb33a
frank198 <460454039@qq.com>
2016-10-25 18:45:12 +0800

    npm test 修改

--- gruntfile.js
+++ gruntfile.js
@@ -1,44 +1,51 @@
-'use strict';
 
-module.exports = function(grunt) {
+module.exports = function(grunt)
+{
 
-  grunt.loadNpmTasks('grunt-mocha-test');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-mocha-test');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  var src = ['test/manager/taskManager.js', 'test/filters/*.js', 
+	const src = ['test/manager/taskManager.js', 'test/filters/*.js',
   'test/remote/*.js', 'test/service/*.js', 'test/modules/*.js', 'test/util/*.js', 'test/*.js'];
 
   // Project configuration.
-  grunt.initConfig({
-    mochaTest: {
-       test: {
-        options: {
-          reporter: 'spec',
-          timeout: 5000,
-          require: 'coverage/blanket'
-        },
-        src: src
-      },
-      coverage: {
-        options: {
-          reporter: 'html-cov',
-          quiet: true,
-          captureFile: 'coverage.html'
-        },
-        src: src
-      }
-    },
-    clean: {
-      "coverage.html" : {
-        src: ['coverage.html']
-      }
-    },
-    jshint: {
-      all: ['lib/*']
-    }
-  });
+	grunt.initConfig(
+		{
+			mochaTest :
+			{
+				test : {
+					options : {
+						reporter : 'spec'
+                        //timeout  : 5000,
+						//require  : 'coverage/blanket'
+					},
+					src : src
+				},
+				coverage : {
+					options : {
+						reporter    : 'html-cov',
+						quiet       : true,
+						captureFile : 'coverage.html'
+					},
+					src : src
+				}
+			},
+			clean :
+			{
+				'coverage.html' : {
+					src : ['coverage.html']
+				}
+			},
+			jshint :
+			{
+				options : {
+					'esversion' : 6
+				},
+				all : ['lib/*']
+			}
+		});
 
   // Default task.
-  grunt.registerTask('default', ['clean', 'mochaTest', 'jshint']);
+	grunt.registerTask('default', ['clean', 'jshint', 'mochaTest']);
 };
